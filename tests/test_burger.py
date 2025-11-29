37bcc1359000fe322be42ae888f126f62cbb2eab
Nikita Budyldin <budnalex@yandex.ru>
2025-11-29 23:53:47 +0300

    Доделаю

---
+++ tests/test_burger.py
@@ -0,0 +1,11 @@
+import pytest
+from praktikum.burger import Burger
+from unittest.mock import Mock
+
+class TestBurger:
+    def mock_get_buns(self):
+        burger = Burger()
+        mock_bun = Mock()
+        burger.get_price
+        
+    def mock_get
